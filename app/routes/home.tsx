32c86ac2f74f628eeff95b2be732b282763ea2f6
Anthony Shin <shinn92dev@gmail.com>
2025-04-17 15:39:25 -0700

    🔧 config: implement tailwind font theme

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,14 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
 
-export function meta({}: Route.MetaArgs) {
-  return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
-  ];
-}
+export const meta = ({}: Route.MetaArgs) => {
+    return [
+        { title: "New React Router App" },
+        { name: "description", content: "Welcome to React Router!" },
+    ];
+};
 
-export default function Home() {
-  return <Welcome />;
-}
+const Home = () => {
+    return <div>Home</div>;
+};
+
+export default Home;
