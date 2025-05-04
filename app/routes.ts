226cf135715dfe483806e1ed0b9abbc5ef84a9b3
Anthony Shin <shinn92dev@gmail.com>
2025-05-04 16:04:18 -0700

    ✨ ✨ feat: implement contact page layout with buttons

--- app/routes.ts
+++ app/routes.ts
@@ -1,3 +1,7 @@
 import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
-export default [index("routes/home.tsx"), route("about", "routes/about.tsx")] satisfies RouteConfig;
+export default [
+    index("routes/home.tsx"),
+    route("about", "routes/about.tsx"),
+    route("contact", "routes/contact.tsx"),
+] satisfies RouteConfig;
---
+++ app/routes/contact.tsx
@@ -0,0 +1,53 @@
+import { FaGithub, FaEnvelope, FaLinkedin } from "react-icons/fa6";
+import { Button } from "~/components/ui/button";
+
+const Contact = () => {
+    return (
+        <div className="pb-10">
+            <div className="w-full text-center bg-gray-700 py-8">
+                <p>Header</p>
+            </div>
+            <div>
+                <div className="w-full aspect-[1/0.43] bg-amber-200">
+                    <img src="" alt="" />
+                </div>
+            </div>
+            <div>
+                <h1 className="text-7xl font-bold">Contact.</h1>
+                <div className="flex flex-col items-center my-16 mb-26 bg-gray-300/50">
+                    <p>I’m always excited to connect with amazing people 💫 </p>
+                    <p>
+                        Whether it’s a project, volunteer work, or any fun opportunity — count me
+                        in!{" "}
+                    </p>
+                    <p>
+                        Feel free to reach out anytime 📬 I can’t wait to work together with you!
+                        🚀💛
+                    </p>
+                </div>
+                <div className="w-full px-10  mt-10 grid gap-5 md:grid-cols-2">
+                    <Button className="w-full py-7 flex justify-center gap-5 cursor-pointer">
+                        <span className="scale-200">
+                            <FaGithub />
+                        </span>
+                        <p>GitHub</p>
+                    </Button>
+                    <Button className=" w-full py-7 flex justify-center gap-5 cursor-pointer">
+                        <span className="scale-200">
+                            <FaLinkedin />
+                        </span>
+                        <p>LinkedIn</p>
+                    </Button>
+                    <Button className=" w-full py-7 flex justify-center gap-5 cursor-pointer md:col-span-2">
+                        <span className="scale-200">
+                            <FaEnvelope />
+                        </span>
+                        <p>anthony.seunghwan.shin@gmail.com</p>
+                    </Button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Contact;
