8b951c0a6ecf8d4b2ae768d9774e250c026a538d
Anthony Shin <shinn92dev@gmail.com>
2025-05-16 15:55:18 -0700

    🔧 chore: add taskmaster related file into gitignore

---
+++ app/contents/home.ts
@@ -0,0 +1,21 @@
+import { IoDocumentAttach } from "react-icons/io5";
+import { FaGithub, FaLinkedin } from "react-icons/fa6";
+import { MdEmail } from "react-icons/md";
+
+const HOME_CONTENT = {
+    title: "Anthony Shin",
+    titleMsg: "Hello There!👋",
+    titleMsg2: "I am ",
+    subTitle: "International and create full-stack developer✨",
+    buttons: [
+        { title: "LinkedIn", icon: FaLinkedin },
+        { title: "GitHub", icon: FaGithub },
+        { title: "Resume", icon: IoDocumentAttach },
+        { title: "Email", icon: MdEmail },
+    ],
+    shortIntro:
+        "a Result-oriented full-stack developer🛠️ with a global perspective🌍 and a passion for turning complexity into clarity.🧠",
+    projectTitle: "SOME OF MY RECENT WORKS",
+};
+
+export default HOME_CONTENT;
