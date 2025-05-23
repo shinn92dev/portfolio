7363a55c267a101dfac6b38d28940ed4a4175741
Anthony Shin <shinn92dev@gmail.com>
2025-05-22 23:33:10 -0700

    📝 doc: add about education lang

---
+++ app/contents/en/about_education.ts
@@ -0,0 +1,25 @@
+const ABOUT_EDUCATION = [
+    {
+        id: 1,
+        name: "British Columbia Institute of Technology (BCIT)",
+        degree: "Diploma in Computer System Technology (CST)",
+        location: "Vancouver, BC, Canada",
+        duration: "2023 - 2025",
+    },
+    {
+        id: 2,
+        name: "Korean National Open University",
+        degree: "Bachelor of Computer Science",
+        location: "Seoul, South Korea",
+        duration: "2021 - 2022",
+    },
+    {
+        id: 3,
+        name: "Kwangwoon University",
+        degree: "Bachelor of International Studies",
+        location: "Seoul, South Korea",
+        duration: "2010 - 2019",
+    },
+];
+
+export default ABOUT_EDUCATION;
