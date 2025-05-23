0211bf528b399b3b6b05a3c9f1ea0aef45d60444
Anthony Shin <shinn92dev@gmail.com>
2025-05-23 00:54:50 -0700

    🚧 doc: add more experience content on lang file

--- app/routes/about.tsx
+++ app/routes/about.tsx
@@ -1,24 +1,41 @@
 import AboutHeader from "@/components/custom/AboutHeader";
 import { useHeaderHeight } from "@/components/custom/context/HeaderHeightContext";
 import ABOUT_EDUCATION from "@/contents/en/about_education";
-import EducationCard from "~/components/custom/EducationCard";
+import ABOUT_EXPERIENCES from "@/contents/en/about_experiences";
+import EducationCard from "@/components/custom/EducationCard";
+import ExperienceCard from "@/components/custom/ExperienceCard";
+
 const about = () => {
     const headerHeight = useHeaderHeight();
     return (
-        <div className="pb-10">
+        <div className="mb-10">
             <AboutHeader pt={headerHeight} />
-            <div className="min-h-screen h-[1024px]">
-                <section id="about" className="h-[1000px] border-2 my-2">
+            <div className="grid gap-y-5">
+                <section id="about" className="border-2 ">
                     About
                 </section>
-                <section id="detailed_about" className="h-[1000px] border-2 my-2">
+                <section id="detailed_about" className="border-2 ">
                     Detailed
                 </section>
-                <section id="experience" className="h-[1000px] border-2 my-2">
-                    Experience
+                <section id="experience" className="">
+                    <h2 className="text-2xl font-bold mb-5">Experiences</h2>
+                    <div className="grid gap-y-2 md:px-5 px-3">
+                        {ABOUT_EXPERIENCES.map((item) => {
+                            return (
+                                <ExperienceCard
+                                    key={item.id}
+                                    company={item.company}
+                                    title={item.title}
+                                    location={item.location}
+                                    duration={item.duration}
+                                    role={item.role}
+                                />
+                            );
+                        })}
+                    </div>
                 </section>
-                <section id="skill" className="my-5"></section>
-                <section id="education" className="my-5">
+                <section id="skill" className=""></section>
+                <section id="education" className="">
                     <h2 className="text-2xl font-bold mb-5">Education</h2>
                     <div className="grid gap-y-2 md:px-5 px-3">
                         {ABOUT_EDUCATION.map((item) => {
