34dfbabc0044be62b23f9651cb19e16a10fe0e21
Anthony Shin <shinn92dev@gmail.com>
2025-05-27 13:48:47 -0700

    📝 doc: add about skill doc

---
+++ app/contents/en/about_skills.ts
@@ -0,0 +1,28 @@
+const ABOUT_SKILLS = {
+    programmingLanguage: [
+        "JavaScript",
+        "TypeScript",
+        "Python",
+        "Java",
+        "C",
+        "HTML",
+        "CSS",
+        "SCSS",
+        "SQL",
+    ],
+    frontend: ["ReactJS", "React Native", "Next.js", "ReactRouter", "Shadcn/UI", "Tailwind CSS"],
+    backend: ["FastAPI", "Strapi", "Express.js", "Django", "Flask", "Firebase"],
+    authenticationSecurity: ["Clerk", "Firebase Authentication", "JWT", "OAuth", "SSL"],
+    database: ["MySQL", "PostgreSQL", "SQLite", "MongoDB", "Firestore", "Prisma", "SQLAlchemy"],
+    devOpsInfra: [
+        "Docker",
+        "Nginx",
+        "Ubuntu Server",
+        "Git/Git CLI",
+        "GitHub",
+        "DigitalOcean",
+        "Vercel",
+    ],
+};
+
+export default ABOUT_SKILLS;
