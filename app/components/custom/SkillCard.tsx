7670d70578aeca2e334c133459ba008fa5f3ab91
Anthony Shin <shinn92dev@gmail.com>
2025-05-27 13:47:44 -0700

    ✨ feat: implement skill card component

---
+++ app/components/custom/SkillCard.tsx
@@ -0,0 +1,19 @@
+import { Card, CardTitle } from "../ui/card";
+import SKILL_LOGOS from "@/contents/skillLogo";
+interface SkillCardProps {
+    skill: string;
+}
+
+const SkillCard: React.FC<SkillCardProps> = ({ skill }) => {
+    let skillKey = skill.toLowerCase().replace(" ", "").replace(".", "").replace("/", "");
+    return (
+        <Card className="w-42">
+            <div className="flex flex-col items-center gap-y-5">
+                <img src={SKILL_LOGOS[skillKey]} alt="" className="w-35 h-35" />
+                <CardTitle className="text-center">{skill}</CardTitle>
+            </div>
+        </Card>
+    );
+};
+
+export default SkillCard;
