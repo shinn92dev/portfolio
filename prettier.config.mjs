b0d5c5a86906867c964b8d23f1bd2a3cfb92f57c
Anthony Shin <shinn92dev@gmail.com>
2026-08-19 03:21:59 -0700

    ➕ chore(prettier): configure Prettier formatting

---
+++ prettier.config.mjs
@@ -0,0 +1,12 @@
+/** @type {import("prettier").Config} */
+const config = {
+  plugins: ["prettier-plugin-tailwindcss"],
+  semi: true,
+  singleQuote: false,
+  trailingComma: "all",
+  tabWidth: 2,
+  useTabs: false,
+  printWidth: 80,
+};
+
+export default config;
